60c161698e3c3b129ca25899e2085b03811d9c81
AgileCreativeSolutions <78875991+AgileCreativeSolutions@users.noreply.github.com>
2023-06-01 10:25:03 -0400

    Update rollover-script.js

--- rollover-script.js
+++ rollover-script.js
@@ -1,7 +1,2 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/momenttimezone/0.5.31/moment-timezone-with-data-2012-2022.min.js"></script>
-
-
 const lastdate=moment().endOf('month').format("MM-DD-YYYY");
 document.getElementById("disclaimer-date").innerHTML = "Offer expires " + lastdate;
-
